20c0b91ef49b7443fb036c3b989dbb05a4be9490
睿知 夏 <xrzxrzx@163.com>
2026-03-12 14:36:04 +0800

    备份

--- ChatRoom.Server/ChatRoom.cpp
+++ ChatRoom.Server/ChatRoom.cpp
@@ -3,3 +3,13 @@
 ChatRoom::ChatRoom()
 {
 }
+
+void ChatRoom::Join(const User& user)
+{
+	m_users.push_back(user);
+}
+
+void ChatRoom::Leave(const User& user)
+{
+	m_users.remove(user);
+}
--- ChatRoom.Server/User.cpp
+++ ChatRoom.Server/User.cpp
@@ -1,5 +1,11 @@
 #include "User.h"
 
+User::User(const std::string& name, int id)
+{
+    m_name = name;
+	m_id = id;
+}
+
 std::string User::GetName() const
 {
     return m_name;
